dc944dc0ac30718205f1b611738c2365838ce1af
olyflower <2013.olga.yudina@gmail.com>
2023-05-23 13:50:19 +0300

    Add changes

--- src/craft/views.py
+++ src/craft/views.py
@@ -186,3 +186,4 @@ class OrderDetailView(View):
         order_items = order.order_items.all()
         context = {"order": order, "order_items": order_items}
         return render(request, "craft/order_success.html", context)
+
